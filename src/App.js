5522ce0dd6c8be7a29362475e094d68f01609113
2hubrulee <alejandro2hpxa@gmail.com>
2018-04-04 21:59:50 -0500

    react bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import logo from './teacher.svg';
 import './App.css';
 import data from "./data.json";
+import { Button } from 'react-bootstrap';
 import BoxGrid from './BoxGrid';
 
 class App extends Component {
@@ -57,7 +58,7 @@ class App extends Component {
                 selection:this.state.selection,
         };
         axios.post('http://127.0.0.1:8000/disponibilidad/api',data).then(function (response) {
-            console.log('salvado')
+            console.log('salvado en la base de datos')
         })
     }
 
@@ -66,11 +67,34 @@ class App extends Component {
         const { select } = this
       const { rows,columns,selection,enabled } = this.state
     return (
+
       <div className="App">
         <header className="App-header">
-            <h1 className="App-title">  Disponibilidad del docente</h1>
-            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title"><img src={logo} className="App-logo" alt="logo" />
+                <div>Disponibilidad del docente</div></h1>
         </header>
+          <Navbar>
+              <Navbar.Header>
+                  <Navbar.Brand>
+                      <a href="#home">React-Bootstrap</a>
+                  </Navbar.Brand>
+              </Navbar.Header>
+              <Nav>
+                  <NavItem eventKey={1} href="#">
+                      Link
+                  </NavItem>
+                  <NavItem eventKey={2} href="#">
+                      Link
+                  </NavItem>
+                  <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                      <MenuItem eventKey={3.1}>Action</MenuItem>
+                      <MenuItem eventKey={3.2}>Another action</MenuItem>
+                      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                      <MenuItem divider />
+                      <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                  </NavDropdown>
+              </Nav>
+          </Navbar>;
           <div>
               <h1 className="App-sub-title">Disponibilidad de horario</h1>
               <BoxGrid rows={rows} columns={columns} selection={selection} enabled={enabled} onSelect={select}/>
